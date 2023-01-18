62ebee05c5d8b0db9a6fd30d1968d364659f2ce1
heesoooo <3628gmltn@gmail.com>
2023-01-18 10:54:06 +0900

    버튼 스타일 수정

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,15 @@
 
 		<Container :posting="posting" :step="step" :image="image" @write="write = $event" />
 
-		<button v-if="step == 0, btnMore==true" @click="more" class="btn-more">더보기</button>
-
-		<div class="footer" v-if="step == 0">
+		<button v-if="step == 0 && btnMore==true" @click="more" class="btn-more">더보기 ▼</button>		
+		
+		<div class="footer" v-if="step == 0">			
 			<ul class="footer-button-plus">
 				<li>
 					<input @change="upload" accept="image/*" type="file" id="file" class="inputfile" />
-					<label for="file" class="input-plus">+</label>
+					<label for="file" class="input-plus">➕ 게시물추가</label>
 				</li>
-			</ul>
+			</ul>		
 		</div>
 	</div>
 </template>
@@ -44,9 +44,13 @@
 				selectFilter: '',
 				카운터: 0,
 				btnMore: true,
+				isShow: true,
 			}
 		},
 		methods: {
+			isToggle() {
+				this.isShow = !this.isShow;
+			},
 			publish() {
 				var mynotice = {
 					name: "Minny",
@@ -103,10 +107,11 @@
 
 <style>
 	body {
-		margin: 0;
+		margin: 0; padding: 0;
 	}
 
 	ul {
+		margin: 0;
 		padding: 5px;
 		list-style-type: none;
 	}
@@ -152,17 +157,15 @@
 		width: 100%;
 		position: sticky;
 		bottom: 0;
-		padding-bottom: 10px;
+		border-top:1px solid #ddd;
 		background-color: white;
 	}
 
 	.footer-button-plus {
-		width: 80px;
-		margin: auto;
+		padding: 0;
 		text-align: center;
 		cursor: pointer;
 		font-size: 24px;
-		padding-top: 12px;
 	}
 
 	.sample-box {
@@ -176,14 +179,18 @@
 	}
 
 	.input-plus {
+		display: block;
+		width: 100%;
+		height: 50px;
+		line-height: 50px;
 		cursor: pointer;
+		font-size: 13px;
 	}
 
 	.btn-more {
 		display: block;
 		outline: none;
 		border: none;
-		margin: 10px 0;
 		padding: 10px 0;
 		width: 100%;
 		cursor: pointer;
